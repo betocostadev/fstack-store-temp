d0c1812030d12b67bf0ea5fa5ebf7b5b9d9a9ec2
betocostadev <robertomarx@gmail.com>
2024-01-17 17:06:06 -0300

    Fixed styled components for SSR

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import nProgress from 'nprogress'
+import Router from 'next/router'
+
 import PropTypes from 'prop-types'
 import Page from '../components/Page'
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => nProgress.start())
+Router.events.on('routeChangeComplete', () => nProgress.done())
+Router.events.on('routeChangeError', () => nProgress.done())
 
 export default function App({ Component, pageProps }) {
   return (
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Document, { Head, Html, NextScript, Main } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class Doc extends Document {
+  // static getInitialProps is only a fix for Styled Components when using SSR
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    )
+    const styleTags = sheet.getStyleElement()
+
+    return { ...page, styleTags }
+  }
+
   render() {
     return (
       <Html lang="en">
