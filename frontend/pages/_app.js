6a8f4830e39314b22b223340306e09aeb5fb98f3
betocostadev <robertomarx@gmail.com>
2024-02-02 20:22:46 -0300

    Added Apollo boiler plate to work with next

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,23 +2,40 @@
 import nProgress from 'nprogress'
 import Router from 'next/router'
 
+import { ApolloProvider } from '@apollo/client'
 import PropTypes from 'prop-types'
 import Page from '../components/Page'
 import '../components/styles/nprogress.css'
+import withData from '../lib/withData'
 
 Router.events.on('routeChangeStart', () => nProgress.start())
 Router.events.on('routeChangeComplete', () => nProgress.done())
 Router.events.on('routeChangeError', () => nProgress.done())
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
+}
+
+// Apollo boiler plate to handle pageProps
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {}
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+  pageProps.query = ctx.query
+  return pageProps
 }
 
 App.propTypes = {
   Component: PropTypes.func,
   pageProps: PropTypes.any,
+  apollo: PropTypes.object,
 }
+
+export default withData(App)
